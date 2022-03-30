4cc1b67b42e4b22425d579f6887702bb158efcb7
Kristoffer Brabrand <kristoffer@brabrand.no>
2022-03-30 15:30:24 +0200

    Adjust typing to better match the actual behavior of the replace function. Now leaves casing as-is, except for uppercasing the first letter after an underscore

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 type CamelCase<S extends string> =
   S extends `${infer P1}_${infer P2}${infer P3}`
-    ? `${Lowercase<P1>}${Uppercase<P2>}${CamelCase<P3>}`
-    : Lowercase<S>;
+    ? `${P1}${Uppercase<P2>}${CamelCase<P3>}`
+    : S;
+
+
 
 export type Camelize<T> = {
   [K in keyof T as CamelCase<string & K>]: T[K] extends Array<infer U>
